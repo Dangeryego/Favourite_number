85f2c709695d41b75e0a3590f92dd94edc8b6969
Muaz Bin Gulfam <106175817+Dangeryego@users.noreply.github.com>
2022-07-30 01:22:10 +0500

    Add files via upload
    For code: Open main.cpp file

---
+++ Favourite Number/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << " \n\n\t\t Enter Your Favourite Number  ";
+    cin >> n;
+    cout << n << "!! " << endl;
+    cout << "Amazing! thats my favourite number too\n No really that's my favourite number too "<< endl;
+    return 0;
+}
